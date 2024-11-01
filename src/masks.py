d643e00f932df139830e8c0c4e3a59a5a953707b
Lyudmila Smirnova <lyuddmila@mail.ru>
2024-11-01 22:59:34 +0300

    homework_9.1

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def (number_card: str) -> str:
+    """Функция маскировки номера банковской карты"""
+    return number_card[:4] + " " + number_card[5:7] + "** **** " + number_card[-4:]
+
+
+print(get_mask_card_number("7000792289606361"))
+
+
+def get_mask_account(number_account: str) -> str:
+    """Функция маскировки номера банковского счета"""
+    return "**" + number_account[-4:]
+
+
+print(get_mask_account("73654108430135874305"))
