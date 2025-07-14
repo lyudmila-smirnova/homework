d566fccd2e35d0f51f906350f9548609c6b47139
lyudmila_s <s.lyuddmila.s@gmail.com>
2025-07-14 13:02:02 +0300

    bug_fixes_3

--- src/processing.py
+++ src/processing.py
@@ -9,17 +9,6 @@ def filter_by_state(list_data: list, state: str = "EXECUTED") -> list:
     return filter_list_data
 
 
-list_data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
-print(filter_by_state(list_data, state="CANCELED"))
-
-
 def sort_by_date(list_data: list, reverse: bool = True) -> list:
     """Функция принимает на вход список словарей и параметр порядка сортировки,
     возвращает новый список, в котором исходные словари отсортированы по дате."""
@@ -27,4 +16,12 @@ def sort_by_date(list_data: list, reverse: bool = True) -> list:
     return sort_list_data_revers
 
 
+list_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]  # входные данные для проверки функции
+
+print(filter_by_state(list_data, state="CANCELED"))
 print(sort_by_date(list_data, reverse=False))
