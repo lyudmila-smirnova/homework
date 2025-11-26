9a65ebfb0d1c07ab1625583e4c856a0d1c517beb
lyudmila_s <s.lyuddmila.s@gmail.com>
2025-11-26 22:55:03 +0300

    add test_log_to_console_succes

---
+++ tests/test_decorators.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+from decorators import log
+
+
+def test_log_to_console_success(capsys):
+    """Тест 1: Успешное выполнение функции с выводом в консоль"""
+
+    @log()  # Без filename - вывод в консоль
+    def add(a, b):
+        return a + b
+
+    result = add(2, 3)
+
+    # Проверяем что вывелось в консоль
+    captured = capsys.readouterr()
+
+    assert result == 5
+    assert "add ok" in captured.out
+
